511e2f53e22c9ee85071deb7b154e597dee9e88a
Felipe Cadar <cadar@dcc.ufmg.br>
2023-06-12 18:17:07 +0200

    fix doc string

--- setup.py
+++ setup.py
@@ -1,18 +1,27 @@
+import sys
 from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert_file('README.md', 'rst')
-except(IOError, ImportError):
-    long_description = open('README.md').read()
+# try:
+#     import pypandoc
+#     long_description = pypandoc.convert_file('README.md', 'rst')
+# except(IOError, ImportError):
+#     long_description = open('README.md').read()
+
+if sys.version_info[0] < 3:
+    with open('README.md') as f:
+        long_description = f.read()
+else:
+    with open('README.md', encoding='utf-8') as f:
+        long_description = f.read()
 
 setup(
     name='easy_local_features',
-    version='0.2.1',
+    version='0.2.2',
     author='eucadar',
     author_email='python@eucadar.com',
     packages=find_packages(exclude=('tests', 'docs', 'assets')),
     include_package_data=True,
     install_requires=[],
+    long_description_content_type='text/markdown',
     long_description=long_description,
 )
