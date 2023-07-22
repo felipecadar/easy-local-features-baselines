a1500e8694696790bea5858b67721c969689b292
Felipe Cadar Chamone <cadar@dcc.ufmg.br>
2023-07-22 10:41:14 +0200

    v034

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ else:
 
 setup(
     name='easy_local_features',
-    version='0.3.3',
+    version='0.3.4',
     author='eucadar',
     author_email='python@eucadar.com',
     packages=find_packages(exclude=('tests', 'docs', 'assets')),
