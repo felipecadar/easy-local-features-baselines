6ed6a901c27b7a030b929eb1e857760fcd981d91
Felipe Cadar <cadar@dcc.ufmg.br>
2024-06-26 22:55:45 +0200

    deploy it

--- setup.py
+++ setup.py
@@ -4,17 +4,33 @@ from setuptools import setup, find_packages
 with open('README.md', encoding='utf-8') as f:
     long_description = f.read()
 
-requires = []
-with open('requirements.txt') as f:
-    requires = f.read().splitlines()
+requires = [
+    "torch>=1.9.1",
+    "torchvision>=0.9.1",
+    "opencv-python",
+    "numpy",
+    "scipy",
+    "wget",
+    "tqdm",
+    "kornia-rs",
+    "kornia_moons",
+    "omegaconf",
+    "pyrootutils",
+    "tensorflow",
+    "tensorflow_hub",
+    "yacs",
+]
+
 
 setup(
     name='easy_local_features',
-    version='0.4.0',
+    version='0.4.1',
     author='eucadar',
     author_email='python@eucadar.com',
     packages=find_packages(exclude=('tests', 'docs', 'assets')),
     include_package_data=True,
+    # include the requirements file
+    package_data={'': ['requirements.txt']},
     install_requires=requires,
     long_description_content_type='text/markdown',
     long_description=long_description,
