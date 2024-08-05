3c4423514759b8126fbe8757fcb092ae8b376d7e
Felipe Cadar <cadar@dcc.ufmg.br>
2024-08-05 10:31:09 -0300

    version bump

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ requires = [
 
 setup(
     name='easy_local_features',
-    version='0.4.12',
+    version='0.4.13',
     author='eucadar',
     author_email='python@eucadar.com',
     packages=find_packages(exclude=('tests', 'docs', 'assets')),
