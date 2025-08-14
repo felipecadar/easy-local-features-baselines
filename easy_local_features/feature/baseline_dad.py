35eb1081d6f2bead76c2083dd361e6420ee0024c
cauamp <cauamp11@gmail.com>
2025-08-14 12:59:34 -0300

    feat: remove unused dad_extract function from baseline_dad.py

--- easy_local_features/feature/baseline_dad.py
+++ easy_local_features/feature/baseline_dad.py
@@ -5,27 +5,6 @@ from .basemodel import BaseExtractor
 from ..matching.nearest_neighbor import NearestNeighborMatcher
 
 
-def dad_extract(img0, img1):
-    H, W = img0.shape[-2:]
-
-    img0 = img0.to("mps")
-    img1 = img1.to("mps")
-    mkpts0 = detector.detect({"image": img0}, num_keypoints=512)["keypoints"]
-    mkpts1 = detector.detect({"image": img1}, num_keypoints=512)["keypoints"]
-
-    mkpts0 = detector.to_pixel_coords(mkpts0, H, W)
-    mkpts1 = detector.to_pixel_coords(mkpts1, H, W)
-
-    # vis.plot_pair(img0=img0, img1=img1)
-    # vis.plot_keypoints(keypoints0=mkpts0.cpu(), keypoints1=mkpts1.cpu())
-    # vis.show()
-
-    return {
-        "mkpts0": mkpts0[0],
-        "mkpts1": mkpts1[0],
-    }
-
-
 class DAD_baseline(BaseExtractor):
     default_conf = {"num_keypoints": 512}
 
